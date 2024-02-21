136dc28d69695a793aba4f3fe9a0925286a740f1
Dawiokkj <davilalongi@gmail.com>
2024-02-21 11:07:30 -0400

    Criação de um metodo POST

--- index.js
+++ index.js
@@ -9,24 +9,42 @@ app.get('/oi', function(req, res){
   res.send('Olá Mundo!')
 })
 
-//Lista de Personagens
+// Lista de Personagens
 const lista = ['Ricky Sanchez', 'Morty Smith', 'Summer Smith']
 
-//Read All -> [GET] item
+// Read All -> [GET] item
 app.get('/item',function (req, res){
   res.send(lista)
 })
 
-//Read by ID -> [GET] item/:id
+// Read by ID -> [GET] item/:id
 app.get('/item/:id', function(req, res){
-  //Acessa o ID no parametro da rota
+  // Acessa o ID no parametro da rota
   const id = req.params.id
 
-  //Acessa o item na lsita baseado no ID recebido
+  // Acessa o item na lsita baseado no ID recebido
   const item = lista[id]
 
-  //Envia o item recebido como resposta HTTP
+  // Envia o item recebido como resposta HTTP
   res.send(item)
+})
+
+// Sinalizamos que o corpo da requisição está em JSON
+app.use(express.json())
+
+// Create -> [POST] /item
+app.post('/item', function(req, res){
+  // Extrairmos o corpo da requisição
+  const body = req.body
+
+  // Pegamos o nome (string) que foi enviado ao corpo 
+  const item = body.nome
+
+  // Colocamos o nome dentro da lista de itens
+  lista.push(item)
+
+  // Enviamos uma resposta de Sucesso
+  res.send("Item adicionado com sucesso!")
 })
 
 app.listen(3000)
