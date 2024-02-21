909a59257b36f22f8e48be11b25b208f1654be12
Dawiokkj <davilalongi@gmail.com>
2024-02-21 10:39:09 -0400

    Criação de um metodo GET para Read ALL e Read by ID

--- index.js
+++ index.js
@@ -17,4 +17,16 @@ app.get('/item',function (req, res){
   res.send(lista)
 })
 
+//Read by ID -> [GET] item/:id
+app.get('/item/:id', function(req, res){
+  //Acessa o ID no parametro da rota
+  const id = req.params.id
+
+  //Acessa o item na lsita baseado no ID recebido
+  const item = lista[id]
+
+  //Envia o item recebido como resposta HTTP
+  res.send(item)
+})
+
 app.listen(3000)
