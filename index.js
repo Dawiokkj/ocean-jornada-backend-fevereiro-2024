8341146913392df4ea7ee613ae65dce8cbf1aa82
Dawiokkj <davilalongi@gmail.com>
2024-02-27 10:56:21 -0400

    Inclusão do MongoDB

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const { MongoClient } = require("mongodb");
 
-app.get('/', function (req, res) {
-  res.send('Hello, World!')
-})
+const dbUrl = "mongodb+srv://admin:N84eqL3C87RFw40z@cluster0.k4owyta.mongodb.net";
+const dbName = "OceanJornadaBackendFev2024";
 
-app.get('/oi', function(req, res){
-  res.send('Olá Mundo!')
-})
+async function main() {
+  const client = new MongoClient(dbUrl);
+  
+  console.log("Conectando ao Banco de dados");
+  await client.connect();
+  console.log("Banco de dados conectado com sucesso");
 
-// Lista de Personagens
-const lista = ['Ricky Sanchez', 'Morty Smith', 'Summer Smith']
+  const app = express();
 
-// Read All -> [GET] item
-app.get('/item',function (req, res){
-  res.send(lista)
-})
+  app.get("/", function (req, res) {
+    res.send("Hello, World!");
+  });
 
-// Read by ID -> [GET] item/:id
-app.get('/item/:id', function(req, res){
-  // Acessa o ID no parametro da rota
-  const id = req.params.id
+  app.get("/oi", function (req, res) {
+    res.send("Olá Mundo!");
+  });
 
-  // Acessa o item na lsita baseado no ID recebido
-  const item = lista[id]
+  // Lista de Personagens
+  const lista = ["Ricky Sanchez", "Morty Smith", "Summer Smith"];
 
-  // Envia o item recebido como resposta HTTP
-  res.send(item)
-})
+  // Read All -> [GET] item
+  app.get("/item", function (req, res) {
+    res.send(lista);
+  });
 
-// Sinalizamos que o corpo da requisição está em JSON
-app.use(express.json())
+  // Read by ID -> [GET] item/:id
+  app.get("/item/:id", function (req, res) {
+    // Acessa o ID no parametro da rota
+    const id = req.params.id;
 
-// Create -> [POST] /item
-app.post('/item', function(req, res){
-  // Extrairmos o corpo da requisição
-  const body = req.body
+    // Acessa o item na lsita baseado no ID recebido
+    const item = lista[id];
 
-  // Pegamos o nome (string) que foi enviado ao corpo 
-  const item = body.nome
+    // Envia o item recebido como resposta HTTP
+    res.send(item);
+  });
 
-  // Colocamos o nome dentro da lista de itens
-  lista.push(item)
+  // Sinalizamos que o corpo da requisição está em JSON
+  app.use(express.json());
 
-  // Enviamos uma resposta de Sucesso
-  res.send("Item adicionado com sucesso!")
-})
+  // Create -> [POST] /item
+  app.post("/item", function (req, res) {
+    // Extrairmos o corpo da requisição
+    const body = req.body;
 
-app.listen(3000)
+    // Pegamos o nome (string) que foi enviado ao corpo
+    const item = body.nome;
+
+    // Colocamos o nome dentro da lista de itens
+    lista.push(item);
+
+    res.send("Item adicionado com sucesso!");
+  });
+
+  app.listen(3000);
+}
+
+main();
